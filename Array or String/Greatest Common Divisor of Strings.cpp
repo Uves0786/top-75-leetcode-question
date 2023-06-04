043011a5b22d78a210b09d6c77cb678632bf0b41
Uves0786 <114132833+Uves0786@users.noreply.github.com>
2023-06-04 09:35:37 +0530

    top 75 leetcode question

---
+++ Array or String/Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,14 @@
+//Greatest Common Divisor of Strings
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/
+
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 == str2) return str1;
+        if(str1 + str2 != str2 + str1) return "";
+        if(str1.size() < str2.size()) str2 = str2.substr(str1.size());
+        if(str2.size() < str1.size()) str1 = str1.substr(str2.size());
+        return gcdOfStrings(str1,str2);
+    }
+};
